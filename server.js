bc7a31a86520138a23566178f59610866f4898a9
dave ready <davis_ready@hotmail.com>
2021-02-08 12:27:28 -0500

    finished setting up server

--- server.js
+++ server.js
@@ -7,3 +7,16 @@ const path = require('path');
 
 var app = express();
 var PORT = process.env.PORT || 8080;
+
+//set up express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
+
+//start the server
+app.listen(PORT, function() {
+    console.log("You are now listening on port: " + PORT);
+});
